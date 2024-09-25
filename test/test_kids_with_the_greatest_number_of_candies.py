b11fa66851e355a1c3ef5f504998ab4d9c2de59d
Maria Gabriela Moreno Exposito <marimorexx@gmail.com>
2024-09-25 11:19:43 +0200

    Feat: 1431_kids_with_the_greatest_number_of_candies

---
+++ test/test_kids_with_the_greatest_number_of_candies.py
@@ -0,0 +1,18 @@
+import unittest
+from arrays.kids_with_the_greatest_number_of_candies import Solution
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution() 
+
+    def test_kids_with_candies(self):
+        result = self.solution.kidsWithCandies([2,3,5,1,3], 3)
+        self.assertEqual(result, [True,True,True,False,True])
+
+        result = self.solution.kidsWithCandies([4,2,1,1,2], 1)
+        self.assertEqual(result, [True,False,False,False,False])
+
+        result = self.solution.kidsWithCandies([12,1,12], 10)
+        self.assertEqual(result, [True,False,True]
+)
