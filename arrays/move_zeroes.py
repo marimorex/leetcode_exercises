5e0dfdf571178ee06abd211450bdaac6fd788e10
Maria Gabriela Moreno Exposito <marimorexx@gmail.com>
2024-09-27 15:07:41 +0200

    Feat: 283_move_zeroes

---
+++ arrays/move_zeroes.py
@@ -0,0 +1,43 @@
+'''
+LeetCode Exercise URL : https://leetcode.com/problems/move-zeroes/
+
+Given an integer array nums, move all 0's to the end of it 
+while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+'''
+
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> List[int]:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zeros = []
+        
+        for i in range(len(nums)):
+            num = nums[i]
+            if(num==0):
+                # store the zero position
+                zeros.append(i)
+            else:
+                # check if we can swapp if we have available zeroes
+                if(len(zeros)>0):
+                   nums[zeros.pop(0)] = num
+                   nums[i] = 0
+                   zeros.append(i)
+        return nums
+
+            
---
+++ test/test_move_zeroes.py
@@ -0,0 +1,23 @@
+import unittest
+
+from arrays.move_zeroes import Solution
+
+class TestMoveZeroes(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_move_middle_zeroes(self):
+        result = self.solution.moveZeroes([0,1,0,3,12])
+        self.assertEqual(result,[1,3,12,0,0])
+    
+    def test_move_empty_list(self):
+        result = self.solution.moveZeroes([0])
+        self.assertEqual(result,[0])
+
+    def test_move_two_zeroes_list(self):
+        result = self.solution.moveZeroes([0,0])
+        self.assertEqual(result,[0,0])
+
+    def test_move_start_zeroes_list(self):
+        result = self.solution.moveZeroes([0,0,1,2,3])
+        self.assertEqual(result,[1,2,3,0,0])
