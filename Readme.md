d6384b9effe29eebddcffe3d6c49e70771cf573e
Maria Gabriela Moreno Exposito <marimorexx@gmail.com>
2024-10-09 11:56:14 +0200

    docs: add sphinx documentation. Add docstring to can_place_flowers

--- Readme.md
+++ Readme.md
@@ -4,25 +4,42 @@
 
 # Instructions to Contribute
 
-1. **Clone the repository**:
+1. **Prerequisites:**
+- python3, pip3 and venv. here after some commands to install the prerequisits:
+    - `sudo apt update`
+    - `sudo apt install python3`
+    - `sudo apt install python3-pip`
+    - `sudo apt install python3-venv`
+
+2. **Clone the repository**:
    `git clone https://github.com/marimorex/leetcode_exercises.git`
 
-2. **Navigate to the project folder**:
+3. **Navigate to the project folder**:
    `cd /path/to/project`
 
-3. **Create a virtual environment**:
-   `python3 -m venv venv`
+4. **Create a virtual environment**:
+   `python3 -m venv name_of_your_venv`
 
-4. **Activate the virtual environment**:
-   - For Linux/macOS: `source venv/bin/activate`
+5. **Activate the virtual environment**:
+   - For Linux/macOS: `source name_of_your_venv/bin/activate`
+        - To deactivate venv run: `deactivate`
+            - to remove a venv run : `sudo rm -rf name_of_your_venv`
    - For Windows: `venv\Scripts\activate`
 
-5. **Install the dependencies**:
+6. **Install the dependencies**:
    `pip install -r requirements.txt`
 
-6. **Run tests**:
+7. **Run tests**:
    `python3 -m unittest discover test`
 
-7. **Run pre-commit hooks before making a commit**:
+8. **Run pre-commit hooks before making a commit**:
    Ensure that all files pass pre-commit checks with:
    `pre-commit run --all-files`
+
+9. **Generate Documentation with Sphinx**
+   - To generate documentation for a package from the Python docstring documentation:
+      - In the root directory, run: `sphinx-apidoc -o docs nameOfThePackageToGenerateDocumentationFor`
+         - Example: `sphinx-apidoc -o docs arrays`
+
+   - To generate HTML documentation from the previous Sphinx package documentation:
+      - In the docs directory, run: `make clean html`
--- arrays/can_place_flowers.py
+++ arrays/can_place_flowers.py
@@ -1,31 +1,57 @@
-"""
-LeetCode Exercise URL : https://leetcode.com/problems/can-place-flowers
-
-Description :
-You have a long flowerbed in which some of the plots are planted,
-and some are not. However, flowers cannot be planted in adjacent plots.
-
-Given an integer array flowerbed containing 0's and 1's, where 0 means empty
-and 1 means not empty, and an integer n, return true if n new flowers can
-be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
-
-Example 1:
-Input: flowerbed = [1,0,0,0,1], n = 1
-Output: true
-
-Example 2:
-Input: flowerbed = [1,0,0,0,1], n = 2
-Output: false
-
-"""
-
 from typing import List
 
 
 class Solution:
+    """
+    This class provides a solution to the LeetCode problem:
+    "605 Can Place Flowers" (https://leetcode.com/problems/can-place-flowers).
+
+    Problem Description:
+        You have a long flowerbed in which some plots are planted, and some are not.
+        However, flowers cannot be planted in adjacent plots. Given an integer array
+        flowerbed where 0 represents an empty plot and 1 represents a plot with flowers,
+        and an integer n representing the number of new flowers you need to plant, return
+        True if n new flowers can be planted without violating the no-adjacent-flowers rule.
+        Otherwise, return False.
+
+    Example:
+        Input: flowerbed = [1,0,0,0,1], n = 1
+        Output: True
+
+        Input: flowerbed = [1,0,0,0,1], n = 2
+        Output: False
+
+    Constraints:
+        1 <= flowerbed.length <= 2 * 10^4
+        flowerbed[i] is 0 or 1.
+        There are no two adjacent flowers in flowerbed.
+        0 <= n <= flowerbed.length
+    """
+
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        # update the flowebed, add zeros in the edges, to process all positions with the same logic
-        # left and right and current position must be 0.
+        """
+        Determines if 'n' new flowers can be planted in the flowerbed without
+        violating the rule that no two flowers can be planted in adjacent plots.
+
+        Parameters
+        ----------
+        flowerbed : List[int]
+            A list of integers where 0 represents an empty plot and 1 represents a plot with a flower.
+        n : int
+            The number of new flowers that need to be planted.
+
+        Returns
+        -------
+        bool
+            True if 'n' new flowers can be planted without violating the rule, otherwise False.
+
+        Approach
+        --------
+        - The method extends the flowerbed by adding 0 at both ends to simplify boundary checks.
+        - It iterates through the flowerbed and checks if the current plot, along with its adjacent plots, are empty.
+        - If so, a flower is planted, and the count is incremented.
+        - The method returns True if the number of planted flowers meets or exceeds 'n'; otherwise, it returns False.
+        """
         flowerbed_updated = [0] + flowerbed + [0]
         planted = 0
 
